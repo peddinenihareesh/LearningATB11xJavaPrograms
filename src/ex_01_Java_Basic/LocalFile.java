c14b173b48ccb1037e330f48f70ccd39308bc868
user <hareesh.peddineni@gmail.com>
2025-03-17 19:39:48 +0530

    10th march - Commit final

--- src/ex_01_Java_Basic/LocalFile.java
+++ src/ex_01_Java_Basic/LocalFile.java
@@ -5,4 +5,11 @@ public class LocalFile {
         // This is the final that I have written
         System.out.println("Final");
     }
+
+    public static void main(String args) {
+    }
+
+    public static void main(int args) {
+
+    }
 }
