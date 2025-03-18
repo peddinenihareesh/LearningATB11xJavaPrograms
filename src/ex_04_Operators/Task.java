405b3b071395a629b17455c8d96a13021cc5d0b9
user <hareesh.peddineni@gmail.com>
2025-03-18 22:00:09 +0530

    12th March

---
+++ src/ex_04_Operators/Task.java
@@ -0,0 +1,17 @@
+package ex_04_Operators;
+
+public class Task {
+    public static void main(String[] args) {
+
+
+    double x = 10;
+    double y = 10;
+    double z = 10;
+
+    double result = Math.cbrt(Math.pow(x,2) + Math.pow(y, 2) - Math.abs(z));
+
+        System.out.println(result);
+
+    }
+
+}
